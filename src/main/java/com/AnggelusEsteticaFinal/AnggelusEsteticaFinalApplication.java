81334313a31d3ee933b3575c08798b3eec2c4b2c
qmrn <qmrn@SJO-5CG12851XP.ant.amazon.com>
2024-03-11 18:10:14 -0600

    Proyecto Final, mensajes y esqueleto

---
+++ src/main/java/com/AnggelusEsteticaFinal/AnggelusEsteticaFinalApplication.java
@@ -0,0 +1,13 @@
+package com.AnggelusEsteticaFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnggelusEsteticaFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnggelusEsteticaFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AnggelusEsteticaFinal/AnggelusEsteticaFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AnggelusEsteticaFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnggelusEsteticaFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
